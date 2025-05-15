ed56ed90e8cf9efb27fe60bcc905377110971f82
thomas <92831135+boyotata@users.noreply.github.com>
2025-05-15 13:44:59 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-2_s1VHJeQgI', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6tpjugbcg203i2ne90p5foi5hs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientsecret: '11v3pg4eapuljhjbq2rc45da1pdnh5evpcgngl4iio60qllsoiqj'
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
