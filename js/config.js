6c12b18d5225d2c9258fdf9a2512c14beadce768
thomas <92831135+boyotata@users.noreply.github.com>
2025-05-21 23:56:51 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-2_mHTpUum4z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6i2vknd9peudjdkqkml0mlcmqc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
+        userPoolId: 'eu-west-2_Zd6Ibtiio', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3r8u9du9h6cul6t6nb4o5eb2r3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
         region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://2tcml952g1.execute-api.eu-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
